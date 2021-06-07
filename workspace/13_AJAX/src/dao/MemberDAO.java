86a1b283f0db1e35703fed8d6d9295ac9a99f3de
pokrogo2 <pokrogo@gmail.com>
2021-06-07 17:32:18 +0900

    2021-06-07

---
+++ workspace/13_AJAX/src/dao/MemberDAO.java
@@ -0,0 +1,88 @@
+package dao;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+import dto.Member;
+import mybatis.config.DBService;
+
+public class MemberDAO {
+
+	private SqlSessionFactory factory;
+	private static MemberDAO instance;
+	
+	private MemberDAO() {
+		factory = DBService.getInstance().getFactory();
+	}
+	
+	public static MemberDAO getInstance() {
+		if (instance == null) {
+			instance = new MemberDAO();
+		}
+		return instance;
+	}
+	
+	/* 1. 회원 목록 가져오기 */
+	public List<Member> selectMemberList() {
+		SqlSession ss = factory.openSession();
+		List<Member> list = ss.selectList("dao.member.selectMemberList");
+		ss.close();
+		return list;
+	}
+	public Member selectMemberList(long no) {
+		SqlSession ss = factory.openSession();
+		Member member = ss.selectOne("dao.member.selectMemberByNo", no);
+		ss.close();
+		return member;
+	}
+	
+	public int updateMember(Member member) {
+		SqlSession ss = factory.openSession(false);
+		int result = ss.update("dao.member.updateMember",member);
+		if(result>0) {
+			ss.commit();
+		}
+		ss.close();
+		
+		return result;
+	}
+	public int insertMember(Member member) {
+		SqlSession ss = factory.openSession(false);
+		int result = ss.insert("dao.member.insertMember",member);
+		if(result>0) {
+			ss.commit();
+		}
+		ss.close();
+		
+		return result;
+	}
+	public int deleteMember(long no) {
+		SqlSession ss = factory.openSession(false);
+		int result = ss.delete("dao.member.deleteMember",no);
+		if(result>0) {
+			ss.commit();
+		}
+		ss.close();
+		
+		return result;
+	}
+	public int getMemberCount() {
+		SqlSession ss= factory.openSession();
+		int count = ss.selectOne("dao.member.getMemberCount");
+		ss.close();
+		return count;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ workspace/13_AJAX/src/dto/Paging.java
@@ -0,0 +1,14 @@
+package dto;
+
+public class Paging {
+	private int totalRecord;
+
+	public int getTotalRecord() {
+		return totalRecord;
+	}
+
+	public void setTotalRecord(int totalRecord) {
+		this.totalRecord = totalRecord;
+	}
+	
+}
